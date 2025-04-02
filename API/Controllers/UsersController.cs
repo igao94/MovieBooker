5b9b421dbf0effc2d561ce7980ae1a5a259c7631
igao94 <igao998@gmail.com>
2025-04-02 10:00:00 +0200

    Added endpoint for changing main photo

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Users.Commands.AddPhoto;
 using Application.Users.Commands.DeletePhoto;
+using Application.Users.Commands.SetMainPhoto;
 using Application.Users.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,5 +18,11 @@ public class UsersController : BaseApiController
     public async Task<ActionResult> DeletePhoto(string photoId)
     {
         return HandleResult(await Mediator.Send(new DeletePhotoCommand(photoId)));
+    }
+
+    [HttpPut("set-main-photo/{photoId}")]
+    public async Task<ActionResult> SetMainPhoto(string photoId)
+    {
+        return HandleResult(await Mediator.Send(new SetMainPhotoCommand(photoId)));
     }
 }
---
+++ Application/Users/Commands/SetMainPhoto/SetMainPhotoHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using Domain.Interfaces;
+using MediatR;
+
+namespace Application.Users.Commands.SetMainPhoto;
+
+public class SetMainPhotoHandler(IUnitOfWork unitOfWork,
+    IUserAccessor userAccessor) : IRequestHandler<SetMainPhotoCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(SetMainPhotoCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userAccessor.GetUserWithPhotosAsync();
+
+        var photo = user.UserPhotos.FirstOrDefault(up => up.Id == request.PhotoId);
+
+        if (photo is null) return Result<Unit>.Failure("Photo not found.", 404);
+
+        if (photo.Url == user.PictureUrl) return Result<Unit>.Failure("Photo is already main.", 400);
+
+        user.PictureUrl = photo.Url;
+
+        var result = await unitOfWork.CompleteAsync();
+
+        return result
+            ? Result<Unit>.Success(Unit.Value)
+            : Result<Unit>.Failure("Failed to set main photo.", 400);
+    }
+}
