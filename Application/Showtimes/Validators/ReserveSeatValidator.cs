3bba73f3378cbc2c334d52a0a75eac7f3fa16734
igao94 <igao998@gmail.com>
2025-04-24 12:43:51 +0200

    Added price validation to require exactly two decimal places

--- Application/Showtimes/Validators/ReserveSeatValidator.cs
+++ Application/Showtimes/Validators/ReserveSeatValidator.cs
@@ -14,5 +14,10 @@ public class ReserveSeatValidator : AbstractValidator<ReserveSeatCommand>
             .NotEmpty().WithMessage("Date is required.")
             .Must(date => date > DateTime.UtcNow).WithMessage("Date must be in the future.")
             .Must(date => date.Kind == DateTimeKind.Utc).WithMessage("Date must be in UTC format.");
+
+        RuleFor(r => r.Price)
+            .NotEmpty().WithMessage("Price is reqired.")
+            .Must(p => decimal.Round(p, 2) == p && (decimal.GetBits(p)[3] >> 16) == 2)
+            .WithMessage("Price must have exactly two decimal places(5.00).");
     }
 }
